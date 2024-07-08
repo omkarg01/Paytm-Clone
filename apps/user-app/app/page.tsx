9398fbd943ac52747a4c14638d4fe67cac8d019e
omkarg01 <omkargujja01@gmail.com>
2024-07-08 21:40:04 +0530

    updated

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
-
-import { PrismaClient } from '@repo/db/client'
-const prisma = new PrismaClient()
-
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
 export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <div className="text-3xl">
-      Hi there!
+    <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
     </div>
   );
 }
